95d66c5f21a938d49b384a4af02cde37de814de1
ddoniddoni <psdkei@naver.com>
2020-09-09 16:59:00 +0900

    Update Material-icon and Header

---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
+
+const Header = () => {
+  return (
+    <div>
+      <PersonIcon />
+      <h2>I'm a Header</h2>
+      <ForumIcon />
+    </div>
+  );
+};
+
+export default Header;
